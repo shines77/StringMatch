89a1d4cbb4d40b86ff68b06bd55beec46ddc23a5
shines77 <gz_shines@msn.com>
2018-06-09 18:13:57 +0800

    修正strstr_sse42_1b()的一处小bug, 对性能影响不大;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/main/algorithm/SSEStrStr.h
+++ src/main/algorithm/SSEStrStr.h
@@ -611,7 +611,7 @@ strstr_sse42_v1b(const char_type * text, const char_type * pattern) {
     
     //_mm_store_si128((__m128i *)mask_128i, __mask);
     uint64_t * mask_128i = (uint64_t *)&__mask;
-    if (mask_128i[0] != 0 || mask_128i[1] != 0) {
+    if (likely(mask_128i[0] != 0 || mask_128i[1] != 0)) {
         // The length of pattern is less than kMaxSize (16 or 8).
 #if 1
         text -= kMaxSize;
@@ -754,7 +754,7 @@ STRSTR_MAIN_LOOP:
                     }
                     else {
                         // Restart to search the next char.
-                        offset = 1;
+                        text -= (kMaxSize - 1);
                         goto STRSTR_MAIN_LOOP;
                     }
                 } while (1);
@@ -767,6 +767,152 @@ STRSTR_MAIN_LOOP:
 
     return nullptr;
 }
+
+template <typename char_type>
+static
+SM_NOINLINE_DECLARE(const char_type *)
+strstr_sse42_v1c(const char_type * text, const char_type * pattern) {
+    static const int kMaxSize = SSEHelper<char_type>::kMaxSize;
+    static const int _SIDD_CHAR_OPS = SSEHelper<char_type>::_SIDD_CHAR_OPS;
+
+    static const int kEqualOrdered = _SIDD_CHAR_OPS | _SIDD_CMP_EQUAL_ORDERED
+                                   | _SIDD_POSITIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT;
+
+    static const int kEqualEach = _SIDD_CHAR_OPS | _SIDD_CMP_EQUAL_EACH
+                                | _SIDD_NEGATIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT;
+
+    static const int kEqualEachM = _SIDD_CHAR_OPS | _SIDD_CMP_EQUAL_EACH
+                                 | _SIDD_NEGATIVE_POLARITY | _SIDD_LEAST_SIGNIFICANT
+                                 | _SIDD_UNIT_MASK;
+
+    //alignas(16) uint64_t mask_128i[2];
+    __m128i __text, __pattern, __zero, __mask;
+    int offset;
+    int t_has_null;
+
+    assert(text != nullptr);
+    assert(pattern != nullptr);
+
+#if defined(NDEBUG)
+    (void)(__zero);
+#else
+    __zero = { 0 };
+#endif
+    // Check the length of pattern is less than kMaxSize (16 or 8)?
+    __pattern = _mm_loadu_si128((const __m128i *)pattern);
+
+    // pxor         xmm0, xmm0
+    // pcmpeqb      xmm1, xmm0
+    // pmovmskb     edx,  xmm1
+    __zero = _mm_xor_si128(__zero, __zero);
+    __mask = _mm_cmpeq_epi8(__pattern, __zero);
+    //offset = _mm_movemask_epi8(__mask);
+    
+    //_mm_store_si128((__m128i *)mask_128i, __mask);
+    uint64_t * mask_128i = (uint64_t *)&__mask;
+    if (likely(mask_128i[0] != 0 || mask_128i[1] != 0)) {
+        // The length of pattern is less than kMaxSize (16 or 8).
+        text -= kMaxSize;
+        do {
+            do {
+STRSTR_MAIN_LOOP_16:
+                text += kMaxSize;
+                __text = _mm_loadu_si128((const __m128i *)text);
+                offset = _mm_cmpistri(__pattern, __text, kEqualOrdered);
+                t_has_null = _mm_cmpistrz(__pattern, __text, kEqualOrdered);
+            } while (offset >= kMaxSize && t_has_null == 0);
+
+            if (likely(t_has_null != 0 && offset >= kMaxSize)) {
+                break;
+            }
+            else if (likely(offset != 0)) {
+                text += offset;
+                __text = _mm_loadu_si128((const __m128i *)text);
+
+                __m128i __patt_mask;
+                __patt_mask = _mm_cmpistrm(__zero, __pattern, kEqualEachM);
+                __text = _mm_and_si128(__text, __patt_mask);
+                int full_matched = _mm_cmpistrc(__pattern, __text, kEqualEach);
+                if (likely(full_matched != 0)) {
+                    text -= (kMaxSize - 1);
+                    goto STRSTR_MAIN_LOOP_16;
+                }
+                else {
+                    // Has found
+                    assert(full_matched == 0);
+                    return text;
+                }
+            }
+            else {
+                // Has found
+                assert(offset == 0);
+                return text;
+            }
+        } while (1);
+
+        return nullptr;
+    }
+    else {
+        // The length of pattern is more than or equal kMaxSize (16 or 8).
+        text -= kMaxSize;
+        do {
+STRSTR_MAIN_LOOP:
+            do {
+                text += kMaxSize;
+                __text = _mm_loadu_si128((const __m128i *)text);
+                offset = _mm_cmpistri(__pattern, __text, kEqualOrdered);
+                t_has_null = _mm_cmpistrz(__pattern, __text, kEqualOrdered);
+            } while (offset >= kMaxSize && t_has_null == 0);
+
+            if (likely(t_has_null != 0)) {
+                assert(offset >= kMaxSize);
+                break;
+            }
+            else if (likely(offset != 0)) {
+                assert(t_has_null == 0);
+                assert(offset > 0);
+
+                text += offset;
+
+                const char_type * t = text;
+                const char_type * p = pattern;
+                do {
+                    __m128i __patt, __patt_mask;
+                    __text = _mm_loadu_si128((const __m128i *)t);
+                    __patt = _mm_loadu_si128((const __m128i *)p);
+                    __patt_mask = _mm_cmpistrm(__zero, __patt, kEqualEachM);
+                    __text = _mm_and_si128(__text, __patt_mask);
+                    t += kMaxSize;
+                    p += kMaxSize;
+                    int full_matched = _mm_cmpistrc(__patt, __text, kEqualEach);
+                    int p_has_null = _mm_cmpistrs(__patt, __text, kEqualEach);
+                    if (likely(full_matched == 0)) {
+                        if (likely(p_has_null == 0)) {
+                            // Scan the next part pattern
+                            continue;
+                        }
+                        else {
+                            // Has found, The pattern has null terminator and partial matched.
+                            assert(p_has_null != 0);
+                            break;
+                        }
+                    }
+                    else {
+                        // Restart to search the next char.
+                        text -= (kMaxSize - 1);
+                        goto STRSTR_MAIN_LOOP;
+                    }
+                } while (1);
+            }
+
+            // Has found
+            return text;
+        } while (1);
+    }
+
+    return nullptr;
+}
+
 
 template <typename CharTy>
 class SSEStrStrImpl {
