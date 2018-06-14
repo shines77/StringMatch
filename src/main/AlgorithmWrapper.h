49259963730c8aa9e0c33af108f4df395f99264a
shines77 <gz_shines@msn.com>
2018-06-14 08:44:19 +0800

    移除了一些被注释的代码;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/main/AlgorithmWrapper.h
+++ src/main/AlgorithmWrapper.h
@@ -117,33 +117,6 @@ struct AlgorithmWrapper {
         bool has_compiled() const { return (this->need_preprocessing() ? this->compiled_ : true); }
         bool need_preprocessing() const { return this->algorithm_.need_preprocessing(); }
 
-        /*
-        // Pattern::args
-        tuple_type & get_args() const {
-            return const_cast<tuple_type &>(this->algorithm_.get_args());
-        }
-
-        const tuple_type & get_cargs() const {
-            return this->algorithm_.get_args();
-        }
-
-        void set_args(const tuple_type & args) {
-            this->algorithm_.set_args(args);
-        }
-
-        template <size_t N>
-        inline auto get(const tuple_type & args) const
-            -> decltype(typename std::tuple_element<N, tuple_type>::type) {
-            return std::get<N>(args);
-        }
-
-        template <size_t N>
-        inline void set(const tuple_type & args,
-                        typename std::tuple_element<N, tuple_type>::type && value) const {
-            std::set<N>(args, value);
-        }
-        */
-
         // Pattern::preprocessing()
         bool preprocessing(const char_type * pattern, size_type length) {
             assert(pattern != nullptr);
