2936394dd0bb716d382da3c7b3a92d599360c4e7
shines77 <gz_shines@msn.com>
2018-07-03 01:53:23 +0800

    修正.vcxproj的路径末尾缺少"\"字符的问题;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -673,7 +673,7 @@ int main(int argc, char * argv[])
     printf("\n");
 #endif
 
-#if (defined(_WIN32) || defined(WIN32) || defined(OS_WINDOWS) || defined(__WINDOWS__))
+#if (defined(_WIN32) || defined(WIN32) || defined(OS_WINDOWS) || defined(_WINDOWS_))
     ::system("pause");
 #endif
     return 0;
