6cd1bff4d8e5c10d22179082083af3ed955154d5
shines77 <gz_shines@msn.com>
2018-07-18 19:39:35 +0800

    新增jstd::uchar_traits<T>类;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/main/jstd/char_traits.h
+++ src/main/jstd/char_traits.h
@@ -25,11 +25,73 @@ struct uchar_traits<short> {
     typedef unsigned short type;
 };
 
+template <>
+struct uchar_traits<int> {
+    typedef unsigned int type;
+};
+
+template <>
+struct uchar_traits<long> {
+    typedef unsigned long type;
+};
+
+// jstd::schar_traits<T>
+
+template <typename CharTy>
+struct schar_traits {
+    typedef CharTy type;
+};
+
+template <>
+struct schar_traits<char> {
+    typedef signed char type;
+};
+
+template <>
+struct schar_traits<short> {
+    typedef signed short type;
+};
+
+template <>
+struct schar_traits<int> {
+    typedef signed int type;
+};
+
+template <>
+struct schar_traits<long> {
+    typedef signed long type;
+};
+
+template <>
+struct schar_traits<unsigned char> {
+    typedef signed char type;
+};
+
+template <>
+struct schar_traits<unsigned short> {
+    typedef signed short type;
+};
+
+template <>
+struct schar_traits<unsigned int> {
+    typedef signed int type;
+};
+
+template <>
+struct schar_traits<unsigned long> {
+    typedef signed long type;
+};
+
 // jstd::is_char8<T>
 
 template <typename CharTy>
 struct is_char8 {
     static const bool value = false;
+};
+
+template <>
+struct is_char8<bool> {
+    static const bool value = true;
 };
 
 template <>
