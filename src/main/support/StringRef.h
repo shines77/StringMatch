3176ad2bcf455a343ebffdbc51b7d1a5e319d791
shines77 <gz_shines@msn.com>
2018-08-01 00:30:59 +0800

    完善detail::strlen<T>();
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/main/support/StringRef.h
+++ src/main/support/StringRef.h
@@ -26,7 +26,7 @@ namespace detail {
 
 template <typename CharTy>
 inline std::size_t strlen(const CharTy * str) {
-    return (std::size_t)::strlen(str);
+    return (std::size_t)::strlen((const char *)str);
 }
 
 template <>
@@ -34,16 +34,28 @@ inline std::size_t strlen(const char * str) {
     return (std::size_t)::strlen(str);
 }
 
-#if defined(_WIN32) || defined(WIN32) || defined(OS_WINDOWS) || defined(__WINDOWS__)
 template <>
-std::size_t strlen(const uint16_t * str) {
+inline std::size_t strlen(const unsigned char * str) {
+    return (std::size_t)::strlen((const char *)str);
+}
+
+#if defined(_WIN32) || defined(WIN32) || defined(OS_WINDOWS) || defined(__WINDOWS__)
+
+template <>
+inline std::size_t strlen(const short * str) {
     return (std::size_t)::wcslen((const wchar_t *)str);
 }
+
+template <>
+inline std::size_t strlen(const unsigned short * str) {
+    return (std::size_t)::wcslen((const wchar_t *)str);
+}
+
 #endif // _WIN32
 
 template <>
 inline std::size_t strlen(const wchar_t * str) {
-    return (std::size_t)::wcslen(str);
+    return (std::size_t)::wcslen((const wchar_t *)str);
 }
 
 } // namespace detail
