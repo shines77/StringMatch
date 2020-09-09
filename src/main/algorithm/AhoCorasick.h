d009eb403a3ecb1aee78a4fc1762cb72059659aa
shines77 <gz_shines@msn.com>
2020-09-09 14:07:59 +0800

    [bugfix]: 修正一个小错误
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/main/algorithm/AhoCorasick.h
+++ src/main/algorithm/AhoCorasick.h
@@ -160,7 +160,7 @@ public:
     static bool need_preprocessing() { return true; }
 
     bool is_alive() const {
-        return (this->root_->get() != nullptr);
+        return (this->root_.get() != nullptr);
     }
 
     void init() {
